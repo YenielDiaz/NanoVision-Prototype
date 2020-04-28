3dc1ad595bde95f14fc9cb0d298253ea906d23aa
YenielDiaz <yenie@LAPTOP-KARDRO8G>
2020-04-27 20:57:48 -0400

    Added Particle placeholder
    Added particle placeholder and started working on making the interactable script

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+
+    [SerializeField] float radius = 2f;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
+
+    }
+
+    void TriggerInteraction()
+    {
+      
+    }
+}
