8a74fe4c4308adca321d69e1d43d57b9ddbf6f72
YenielDiaz <yenie@LAPTOP-KARDRO8G>
2020-05-07 18:08:38 -0400

    Made game timer start on command
    For now game timer starts when the K key is pressed

--- Assets/Scripts/GameTimer.cs
+++ Assets/Scripts/GameTimer.cs
@@ -7,7 +7,9 @@ public class GameTimer : MonoBehaviour
 {
     [Tooltip("Our level timer in SECONDS")]
     [SerializeField] float levelTime = 10;
+    [SerializeField] float timeSinceGameStart;
     bool timerFinished = false;
+    bool gameStarted = false;
     Slider slider;
 
     private void Start()
@@ -16,12 +18,26 @@ public class GameTimer : MonoBehaviour
     }
     void Update()
     {
-        /* we divide the time since the level has loaded by
-         the amount of time we want the level to have
-         so that the slider value is proportional to how much time
-         we want the level to last
-         */
-        slider.value = Time.timeSinceLevelLoad / levelTime;
+        //for testing purposes I just say I start the game when I press the K key
+        if (Input.GetKeyDown(KeyCode.K) && !gameStarted)
+        {
+            gameStarted = true;
+            //we store the time at which we started the game
+            timeSinceGameStart = Time.timeSinceLevelLoad;
+        }
+
+
+        //when gameStarted is true then we start the timer
+        if (gameStarted)
+        {
+
+            /* we divide the time since the game has started by
+             the amount of time we want the level to have
+             so that the slider value is proportional to how much time
+             we want the level to last
+            */
+            slider.value = (Time.timeSinceLevelLoad - timeSinceGameStart) / levelTime;
+        }
 
         timerFinished = (Time.timeSinceLevelLoad >= levelTime);
         if (timerFinished)
