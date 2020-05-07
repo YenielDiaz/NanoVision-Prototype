b40958f178e0fb6bc3348ddb78d8d1475d5b13cc
YenielDiaz <yenie@LAPTOP-KARDRO8G>
2020-05-06 23:06:00 -0400

    Handled bead instantiation
    a minimum number of UV beads appear and the beads appear at a random position

--- Assets/Scripts/BeadFactory.cs
+++ Assets/Scripts/BeadFactory.cs
@@ -7,29 +7,53 @@ public class BeadFactory : MonoBehaviour
     [SerializeField] GameObject beadPrefab; 
 
     [SerializeField] int beadCountToCreate;
-    [SerializeField] int minUVBeadCount;
+    [SerializeField] int minUVBeadCount; //MUST BE LESS THAN BEAD COUNT TO CREATE
     [SerializeField] GameObject[] beads;
     [SerializeField] float spacing = 1.5f;
     [SerializeField] Vector3 creationPos = new Vector3(-12, 0, 25);
     System.Random rand = new System.Random(); //we specify System.Random because UnityEngine.Random does not generate random ints
 
+    //starting number of beads set to UV
+    int currUVBeads;
+
     void Start()
     {
-        
+        currUVBeads = 0;
+
         beads = new GameObject[beadCountToCreate];
 
         for (int i=0; i<beadCountToCreate; i++)
         {
             beads[i] = Instantiate(beadPrefab, creationPos, Quaternion.identity);
-            creationPos += new Vector3(spacing, 0, 0);
+            if(currUVBeads < minUVBeadCount)
+            {
+                beads[i].GetComponent<Bead>().SetIsUV(true);
+                currUVBeads++;
+            }
+            //way of positioning beads at random spots, but right now i dont have a way to make sure they dont overlap
+            creationPos.x = rand.Next(-12, 12);
+            creationPos.y = rand.Next(-4, 5);
 
-            ///newUV is true if the randomly generated int is 1 and false if its 0
-            bool newUV = rand.Next(2) == 1 ? true : false; //we gotta alter this so that we ensure a minimum ammount of UV beads
-            beads[i].GetComponent<Bead>().SetIsUV(newUV);
+            /*
+             * 
+             * 
+             * //until we get the min amound of UV beads, we keep turning a random one into UV
+        while (currUVBeads < minUVBeadCount)
+        {
+            if(!beads[rand.Next(0, beadCountToCreate)].GetComponent<Bead>().GetIsUV())
+            {
+                beads[rand.Next(0, beadCountToCreate)].GetComponent<Bead>().SetIsUV(true);
+                currUVBeads++;
+            }
+            
+        }
+             * An Alternative to the above method would be to:
+             *  instantiate the beads outside of the screen with assigned UV vals such that we have a desired amount of UV
+             *  then move their position to a random part of the screen
+             * */
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
         
