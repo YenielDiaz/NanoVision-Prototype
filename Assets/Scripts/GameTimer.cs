2a369bec6618615ae3691dccbfc32e39f774f1ae
YenielDiaz <yenie@LAPTOP-KARDRO8G>
2020-05-06 23:54:50 -0400

    Added Timer
    added timer that countsdown when level loads. Does not do anything else at the moment

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    [Tooltip("Our level timer in SECONDS")]
+    [SerializeField] float levelTime = 10;
+    bool timerFinished = false;
+    Slider slider;
+
+    private void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+    void Update()
+    {
+        /* we divide the time since the level has loaded by
+         the amount of time we want the level to have
+         so that the slider value is proportional to how much time
+         we want the level to last
+         */
+        slider.value = Time.timeSinceLevelLoad / levelTime;
+
+        timerFinished = (Time.timeSinceLevelLoad >= levelTime);
+        if (timerFinished)
+        {
+            //FindObjectOfType<LevelController>().LevelTimerFinished();
+            timerFinished = true;
+        }
+    }
+}
